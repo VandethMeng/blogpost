367733811ab963d79bd273933940e9f6b5bdd2e2
VandethMeng <vandeth.ifl@gmail.com>
2026-02-01 00:24:17 -0600

    change base url

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,16 +3,17 @@ import { NextResponse } from "next/server";
 import { Post } from "@/types/post";
 import { v4 as uuidv4 } from "uuid";
 
+export const dynamic = "force-dynamic"; // <--- Add this!
+
 export async function GET() {
   try {
-    // 1. Await the promise to get the actual client
     const client = await clientPromise;
     const db = client.db("blogpostdb");
-
-    const posts = await db.collection<Post>("posts").find().toArray();
+    const posts = await db.collection("posts").find({}).sort({ createdAt: -1 }).toArray();
     return NextResponse.json({ ok: true, data: posts });
-  } catch (error) {
-    return NextResponse.json({ ok: false, message: "Fetch failed" + error }, { status: 500 });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({ ok: false }, { status: 500 });
   }
 }
 
